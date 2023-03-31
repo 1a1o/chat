afbace840b23a9eb0072a16ceb7e853fe9514cf5
1a1o <l_alo@icloud.com>
2023-03-31 14:24:49 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,6 @@ function Home() {
     <Page className="flex flex-col gap-12">
       <section className="flex flex-col gap-6">
         <Text variant="h1">INNERTEL</Text>
-        <Text className="text-zinc-600">
-          In this example, a simple chat bot is implemented using Next.js, API
-          Routes, and OpenAI API.
-        </Text>
       </section>
 
       <section className="flex flex-col gap-3">
