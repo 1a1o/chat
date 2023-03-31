fcd549c0cde2aab89c60708392ef6fffb761ef2f
1a1o <l_alo@icloud.com>
2023-03-31 17:32:50 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,9 @@ import { Chat } from '../components/Chat'
 function Home() {
   return (
     <Page className="flex flex-col gap-12">
-      <section className="flex flex-col gap-6">
-        <Text variant="h1">INNERTEL</Text>
-      </section>
-
+ 
       <section className="flex flex-col gap-3">
-        <Text variant="h2">AI Chat Bot:</Text>
+ 
         <div className="lg:w-2/3">
           <Chat />
         </div>
